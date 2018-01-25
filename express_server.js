a3877336d651c0494fcf3b7993823f3fe3f4319e
Nic <nic.ehmayer92@gmail.com>
2018-01-25 20:15:06 +0000

    Display username on every view that includes the header.

--- express_server.js
+++ express_server.js
@@ -40,7 +40,11 @@ app.get("/urls", (req, res) => {
 
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    username: req.cookies.username
+  };
+
+  res.render("urls_new", templateVars);
 });
 
 
